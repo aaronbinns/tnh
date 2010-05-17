54e84155633a0b544aedf7afea168c3092f4a50a
Aaron Binns <aaron@archive.org>
2010-05-17 13:36:06 -0700

    Added license header and class-level comments.

--- src/java/Segments.java
+++ src/java/Segments.java
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import java.util.*;
 import java.io.*;
@@ -11,6 +27,13 @@ import org.apache.hadoop.mapred.lib.*;
 
 import org.apache.nutch.parse.ParseText;
 
+/**
+ * Hack to read the parsed text from a Nutch(WAX) segment.  This way
+ * TNH can search indexes created by Nutch(WAX) and also generate
+ * snippets based on the parsed text from the segments.
+ *
+ * 
+ */
 public class Segments
 {
   public static final Logger LOG = Logger.getLogger( Segments.class.getName() );
