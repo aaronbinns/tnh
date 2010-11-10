fb69d408188e9089113508cce82ecedf8a37d722
Aaron Binns <aaron@archive.org>
2010-11-10 11:16:15 -0800

    Control which indexes and fields are emitted.  Emit field info: term counts.

--- src/java/DiagnosticServlet.java
+++ src/java/DiagnosticServlet.java
@@ -22,11 +22,38 @@ import javax.servlet.http.*;
 
 import org.jdom.*;
 
-import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.*;
 import org.apache.lucene.search.*;
 import org.apache.lucene.analysis.*;
 import org.apache.lucene.search.highlight.*;
 
+/**
+ * Simple servlet which emits diagnostic info via an XML Document of
+ * the form:
+ * &lt;info>
+ *  &lt;searcher name="foo" type="org.apache.lucene.search.IndexSearcher">
+ *    &lt;index numDocs="82215">
+ *      &lt;field name="date">
+ *        &lt;term name="2010" count="532" />
+ *        &lt;term name="2009" count="9982" />
+ *      &lt;/field>
+ *    &lt;/index>
+ * &lt;/info>
+ *
+ * The list of indexes and fields to emit ar controlled via URL
+ * parameters <code>i</code> and <code>f</code> respectively.  E.g.
+ * <code>http://localhost/info?i=foo&amp;f=date</code>
+ *
+ * If neither are given, then all indexes are listed, but no fields
+ * are emitted.
+ *
+ * The <code>i</code> and <code>f</code> parameters are otherwise
+ * independent and can be used in any combination.
+ *
+ * Only indexed fields can be emitted, and the only sensible ones are
+ * those with a limited number of terms, otherwise the response is
+ * enormous.  Good fields to emit are: date and type.
+ */
 public class DiagnosticServlet extends HttpServlet
 {
   public static final Logger LOG = Logger.getLogger( OpenSearchServlet.class.getName() );
@@ -35,39 +62,85 @@ public class DiagnosticServlet extends HttpServlet
     throws ServletException, IOException
   {
     Document doc  = new Document();
-    Element  root = new Element( "search" );
+    Element  root = new Element( "info" );
     
     doc.addContent( root );
 
-    Search search = (Search) this.getServletConfig().getServletContext().getAttribute( "tnh.search" );
+    Search search = (Search) this.getServletConfig()
+      .getServletContext()
+      .getAttribute( "tnh.search" );
 
-    if ( search != null )
+    if ( search == null )
       {
-        for ( Map.Entry<String,Searcher> s : search.searchers.entrySet() )
+        OpenSearchHelper.writeResponse( doc, response );
+        
+        return ;
+      }
+
+    Set<String> indexNames = new HashSet( Arrays.asList( ServletHelper.getParam( request, "i", search.searchers.keySet( ).toArray( QueryParameters.EMPTY_STRINGS ) ) ) );
+    Set<String> fieldNames = new HashSet( Arrays.asList( ServletHelper.getParam( request, "f", QueryParameters.EMPTY_STRINGS ) ) );
+    
+    for ( String indexName : indexNames )
+      {
+        Searcher searcher = search.searchers.get( indexName );
+
+        if ( searcher == null ) continue ;
+        
+        Element e = new Element( "searcher" );
+        root.addContent( e );
+        
+        e.setAttribute( "name", indexName );
+        e.setAttribute( "type", searcher.getClass().getCanonicalName() );
+        
+        try
           {
-            String   name     = s.getKey();
-            Searcher searcher = s.getValue();
-
-            Element e = new Element( "searcher" );
-            root.addContent( e );
-
-            e.setAttribute( "name", name );
-            e.setAttribute( "type", searcher.getClass().getCanonicalName() );
-
-            try
-              {
-                IndexSearcher is = (IndexSearcher) searcher;
-                IndexReader   ir = is.getIndexReader();
-
-                Element ise = new Element( "index" );
-                e.addContent( ise );
-
-                ise.setAttribute( "numDocs", Integer.toString( ir.numDocs() ) );
-              }
-            catch ( ClassCastException cce )
-              {
-              }
+            IndexReader ir = ((IndexSearcher) searcher).getIndexReader();
             
+            Element ise = new Element( "index" );
+            e.addContent( ise );
+            
+            ise.setAttribute( "numDocs", Integer.toString( ir.numDocs() ) );
+                        
+            TermDocs termDocs = ir.termDocs( );
+            for ( String fieldName : ir.getFieldNames( IndexReader.FieldOption.ALL ).toArray( QueryParameters.EMPTY_STRINGS ) )
+              {
+                // If this field is not requested, skip it.
+                if ( ! fieldNames.contains( fieldName ) )
+                  {
+                    continue ;
+                  }
+                
+                Element field = new Element( "field" );
+                field.setAttribute( "name", fieldName );
+                ise.addContent( field );
+
+                // Iterate through the terms and for each term that
+                // belongs to this field, count up the number of
+                // documents containing that term and add it to the
+                // XML Document.
+                TermEnum termEnum = ir.terms( new Term( fieldName ) );
+                do
+                  {
+                    Term term = termEnum.term();
+                    
+                    if ( term == null || ! fieldName.equals( term.field() ) ) continue ;
+                    
+                    termDocs.seek( termEnum );
+                    
+                    int c = 0;
+                    for ( ; termDocs.next() ; c++ ) ;
+                    
+                    Element value = new Element( "term" );
+                    value.setAttribute( "name",  term.text() );
+                    value.setAttribute( "count", Integer.toString( c ) );
+                    
+                    field.addContent( value );
+                  }
+                while ( termEnum.next() );
+              }
+          }
+        catch ( ClassCastException cce )
+          {
           }
       }
 
