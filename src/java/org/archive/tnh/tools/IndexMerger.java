c775d8768f10817632665bc2825936bf6ef74d0e
Aaron Binns <aaron@archive.org>
2012-07-24 17:11:07 -0700

    Simplified IndexMerger by using IndexWriter.addIndexes(IndexReader...) instead of addIndex(Directory...).  Even though the Lucene docs say the latter performs better, for us it's actually faster to use the former, which will merge and optimize the input indexes much faster.  Also removed command-line options that no longer apply.

--- src/java/org/archive/tnh/tools/IndexMerger.java
+++ src/java/org/archive/tnh/tools/IndexMerger.java
@@ -28,8 +28,7 @@ import org.apache.lucene.util.*;
  * Handy command-line tool for merging and/or optimizing Lucene
  * indexes.  The default behavior matches the common
  * merge/optimization policies at IA; that is, indexes are merged
- * offline, in one shot.  The termIndexInterval is as in the
- * IndexWriterConfig, unless over-ridden via command-line option.
+ * offline, in one shot.
  */
 public class IndexMerger
 {
@@ -38,21 +37,13 @@ public class IndexMerger
   {
     if ( args.length < 2 )
       {
-        System.err.println( "IndexMerger [-v|-o|-f|-b <size>|-i <value>] <dest> <source>..." );
+        System.err.println( "IndexMerger [-v|-o|-f] <dest> <source>..." );
         System.exit( 1 );
       }
 
     boolean verbose  = false;
     boolean optimize = false;
     boolean force    = false;
-
-    // Default size of RAM buffer (in MB) for merging indexes is max
-    // memory - 12MB.  The Lucene library aborts if the value is not
-    // "comfortably below 2GB" (whatever that means), so we employ a
-    // limit of 1800MB to prevent the Lucene library from aborting.
-    double bufsize = Math.min( (Runtime.getRuntime().maxMemory() / 1024 / 1024) - 12, 1800 );
-
-    int termIndexInterval = IndexWriterConfig.DEFAULT_TERM_INDEX_INTERVAL;
 
     int i = 0;
     for ( ; i < args.length ; i++ )
@@ -69,40 +60,6 @@ public class IndexMerger
           {
             verbose = true;
           }
-        else if ( "-b".equals( args[i] ) )
-          {
-            if ( ++i >= args.length )
-              {
-                System.err.println( "Missing ram buffer size, after -b" );
-                System.exit(1);
-              }
-            try
-              {
-                bufsize = Double.parseDouble( args[i] );
-              }
-            catch ( NumberFormatException nfe )
-              {
-                System.err.println( "Invalid parameter, after -b: " + args[i] );
-                System.exit(1);
-              }
-          }
-        else if ( "-i".equals( args[i] ) )
-          {
-            if ( ++i >= args.length )
-              {
-                System.err.println( "Missing term index interval value, after -i" );
-                System.exit(1);
-              }
-            try
-              {
-                termIndexInterval = Integer.parseInt( args[i] );
-              }
-            catch ( NumberFormatException nfe )
-              {
-                System.err.println( "Invalid parameter, after -i: " + args[i] );
-                System.exit(1);
-              }
-          }
         else
           {
             break ;
@@ -112,7 +69,7 @@ public class IndexMerger
     if ( (args.length - i) < (2 - (optimize ? 1 : 0)) )
       {
         System.err.println( "Erorr: no source files!" );
-        System.err.println( "IndexMerger [-v|-o|-f|-b <size>|-i <value>] <dest> <source>..." );
+        System.err.println( "IndexMerger [-v|-o|-f] <dest> <source>..." );
         System.exit( 1 );
       }
 
@@ -125,45 +82,30 @@ public class IndexMerger
         System.exit( 2 );
       }
     
-    Directory d[] = new Directory[args.length-i];
+    IndexReader ir[] = new IndexReader[args.length-i];
     for ( int j = i ; j < args.length ; j++ )
       {
-        d[j-i] = new MMapDirectory( new File( args[j] ) );
+        ir[j-i] = IndexReader.open( new MMapDirectory( new File( args[j] ) ), true /* read-only */ );
       }
 
     IndexWriter w = null;
     try
       {
-        // Allow for all segments to be merged at once by setting the
-        // max suitably high.  Don't use Integer.MAX_VALUE because the
-        // TieredMergePolicy code does arithematic with this value and
-        // will overflow if set to Integer.MAX_VALUE.
-        TieredMergePolicy mergePolicy = new TieredMergePolicy();
-        mergePolicy.setMaxMergeAtOnceExplicit( 1000000 );
-        mergePolicy.setUseCompoundFile( false );
+        // Configure the IndexWriter.
+        IndexWriterConfig config = new IndexWriterConfig( Version.LUCENE_35, null );
 
-        // Configure the IndexWriter.  Use SerialMergeScheduler so
-        // that the merge/optimize is done all at once, not with
-        // background threads.  Also set the RAM buffer size and the
-        // term interval.
-        IndexWriterConfig config = new IndexWriterConfig( Version.LUCENE_33, null );
-        config.setMergeScheduler( new SerialMergeScheduler() );
-        config.setMergePolicy( mergePolicy );
-        config.setRAMBufferSizeMB( bufsize );
-        config.setTermIndexInterval( termIndexInterval );
         config.setOpenMode( IndexWriterConfig.OpenMode.CREATE_OR_APPEND );
 
         w = new IndexWriter( new MMapDirectory( dest ), config );
         
         if ( verbose )
           {
-            System.err.println( "bufsize = " + bufsize );
             w.setInfoStream( System.out );
           }
 
-        if ( d.length > 0 )
+        if ( ir.length > 0 )
           {
-            w.addIndexes( d );
+            w.addIndexes( ir );
           }
 
         if ( optimize )
